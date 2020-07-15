7aacccd7213828e097d73126d2d11b216ad83921
John Whish <johnwhish@mb5-2015-15-john.lan>
2020-07-15 21:56:35 +0100

    Set up webpack with babel for React

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,31 @@
-const elixir = require("coldbox-elixir");
-const path = require("path");
+const path = require('path');
+const webpack = require('webpack'); //to access built-in plugins
 
-module.exports = elixir(mix => {
-    mix.vue("app.js");
-});
+module.exports = {
+    entry: {
+        main: './resources/assets/js/app.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'includes/js'),
+        filename: '[name].initial.js', /* initial */
+        chunkFilename: '[name].[contenthash].js', /* non-initial chunks */
+        publicPath: 'includes/js/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                resolve: { 
+                    extensions: [".js", ".jsx"] 
+                },
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProgressPlugin()
+    ]
+};
